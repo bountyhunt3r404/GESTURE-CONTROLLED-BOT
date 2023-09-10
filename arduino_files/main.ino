52441ac00d5563281280724e412913af237ef6a8
bountyhun3r404 <bountyhunt3r404@gmail.com>
2023-09-10 10:58:34 +0530

    updated main.ino with latest code

--- arduino_files/main.ino
+++ arduino_files/main.ino
@@ -12,14 +12,28 @@
                                      D27    ->     I4
 */
 
+/*  #################################-PIN CONNECTIONS-#############################################
+                                    ESP32         L293D
+                                     D13    ->     I1
+                                     D12    ->     I2
+                                     D14    ->     I3
+                                     D27    ->     I4
+                                     D25    ->     EN1
+                                     D26    ->     EN2
+
+*/
+
 #define CUSTOM_SETTINGS
 #define INCLUDE_SENSOR_MODULE
 
 #include <DabbleESP32.h>
+#include <Smooth.h>
+
+
 
 //MOTOR PIN CONNECTIONS
-#define enableA 34
-#define enableB 35
+#define enableA 26
+#define enableB 25
 
 #define inputA1 13
 #define inputA2 12
@@ -27,24 +41,18 @@
 #define inputB1 14
 #define inputB2 27
 
-//Limit of 
+//Limit of accelerometer values to neglet
 float const_limit = 3.0;
 
-/*This Function is used to print the Sensor values 
-  recieved from Phone's Accelerometer*/ 
-void print_Accelerometer_data() {
-  Serial.print("X-Axis: ");
-  Serial.print(Sensor.getAccelerometerXaxis(), 4);
-  Serial.print('|');
-  Serial.print("Y-Axis: ");
-  Serial.print(Sensor.getAccelerometerYaxis(), 4);
-  Serial.print('|');
-  Serial.print("Z-Axis: ");
-  Serial.println(Sensor.getAccelerometerZaxis(), 4);
-  delay(10);
-}
+//Setting speed of motors
+int set_speed = 110;
+const int min_motor_speed = 65;
+const int max_motor_speed = 255;
+const int precision = 100;
 
-/*The function */
+
+/*The function takes RAW value of ACCELEROMETER and returns 1 or 0
+  if the value crosses the const_limit*/
 bool dead_zone(float val, float limit = const_limit) {
   if (val >= -limit && val <= limit) {
     return 0;
@@ -55,37 +63,101 @@ bool dead_zone(float val, float limit = const_limit) {
   }
 }
 
+/*This function uses smooth library and takes int and float as INPUT
+  and returns the AVGERAGE VALUE of the same data type.*/
+float smooth_data(float val) {
+  Smooth avg(10);
+
+  if(val < 0) {
+    val *= -1;
+    avg.add(val);
+    
+    return -avg();
+  }
+
+  else {
+    avg.add(val);
+
+    return avg();
+  }
+}
+
+/*###################-BOT MOVEMENT FUNCTIONS-########################*/
 
 void bot_forward() {
   digitalWrite(inputA1, 1);
   digitalWrite(inputA2, 0);
+  analogWrite(enableB, set_speed);
 
   digitalWrite(inputB1, 1);
   digitalWrite(inputB2, 0);
+  analogWrite(enableB, set_speed);
 }
 
 void bot_backward() {
   digitalWrite(inputA1, 0);
   digitalWrite(inputA2, 1);
+  analogWrite(enableB, set_speed);
 
   digitalWrite(inputB1, 0);
   digitalWrite(inputB2, 1);
+  analogWrite(enableB, set_speed);
+
 }
 
 void bot_left() {
   digitalWrite(inputA1, 1);
   digitalWrite(inputA2, 0);
+  analogWrite(enableB, set_speed);
 
   digitalWrite(inputB1, 0);
   digitalWrite(inputB2, 1);
+  analogWrite(enableB, set_speed);
 }
 
 void bot_right() {
   digitalWrite(inputA1, 0);
   digitalWrite(inputA2, 1);
+  analogWrite(enableB, set_speed);
 
   digitalWrite(inputB1, 1);
   digitalWrite(inputB2, 0);
+  analogWrite(enableB, set_speed);
+}
+
+void bot_stop() {
+  digitalWrite(inputA1, 0);
+  digitalWrite(inputA2, 0);
+  analogWrite(enableA, 0);
+
+  digitalWrite(inputB1, 0);
+  digitalWrite(inputB2, 0);
+  analogWrite(enableB, 0);
+}
+
+/*#############################-END-##################################*/
+
+/*This Function is used to print the Sensor values 
+  recieved from Phone's Accelerometer and other parameters
+  such as SMOOTHED VALLUES and SPEED  
+*/ 
+void print_data() {
+  Serial.print("-|");
+  Serial.print("X-Axis: ");
+  Serial.print(Sensor.getAccelerometerXaxis(), 4);
+  Serial.print('|');
+  Serial.print("Y-Axis: ");
+  Serial.print(Sensor.getAccelerometerYaxis(), 4);
+  Serial.print('|');
+  Serial.print("AVG-X: ");
+  Serial.print(smooth_data(Sensor.getAccelerometerXaxis()));
+  Serial.print('|');
+  Serial.print("AVG-Y: ");
+  Serial.print(smooth_data(Sensor.getAccelerometerYaxis()));
+  Serial.print("|");
+  Serial.print("SPEED: ");
+  Serial.println(set_speed);
+  delay(1);
 }
 
 
@@ -104,25 +176,52 @@ void setup() {
 }
 
 void loop() {
-  Dabble.processInput();   //this function is used to refresh data obtained from smartphone.
+  Dabble.processInput();   //This function is used to refresh data obtained from smartphone.
                            //Hence calling this function is mandatory in order to get data properly from your mobile.
-
-  //print_Accelerometer_data();
 
   //Stores the values recieved from the phone into their respective variable names
   float X = Sensor.getAccelerometerXaxis();
   float Y = Sensor.getAccelerometerYaxis();
 
+  X = smooth_data(X);
+  Y = smooth_data(Y);
+
   /*#####################-MOTOR CONTROL SECTION-#########################*/
+
+  //Speed Control of motors
+
+  if (dead_zone(X)) {
+    if (X>0) {
+      set_speed = map(X*precision, 3*precision, 11*precision, min_motor_speed, max_motor_speed);
+    }
+
+    else {
+      set_speed = map(-X*precision, 3*precision, 11*precision, min_motor_speed, max_motor_speed);
+    }
+  }
+
+  else if (dead_zone(Y)) {
+    if (Y>0) {
+      set_speed = map(Y*precision, 3*precision, 11*precision, min_motor_speed, max_motor_speed);
+    }
+    
+    else {
+      set_speed = map(-Y*precision, 3*precision, 11*precision, min_motor_speed, max_motor_speed);
+    }
+  }
+
+  else {
+    set_speed = 0;
+  }
 
   //LEFT-RIGHT SECTION
   if (dead_zone(X)) {
-    if (X > const_limit) {
+    if (X < -const_limit) {
       bot_right();
       Serial.print("RIGHT");
     }
 
-    else if (X < -const_limit) {
+    else if (X > const_limit) {
       bot_left();
       Serial.print("LEFT");
     }
@@ -130,16 +229,24 @@ void loop() {
 
   //FORWARD-BACKWARD SECTION
   else if (dead_zone(Y)) {
-    if (Y > const_limit) {
+    if (Y < -const_limit) {
       bot_forward();
       Serial.print("FORWARD");
     }
 
-    else if (Y < -const_limit) {
+    else if (Y > const_limit) {
       bot_backward();
       Serial.print("BACKWARD");
     }   
   }
 
+  else {
+    bot_stop();
+    Serial.print("STOP");
+  }
+
   /*################################-END-##################################*/
+
+  //Prints the data such as X, Y of accelerometer and other necessary data.
+  print_data();
 }
